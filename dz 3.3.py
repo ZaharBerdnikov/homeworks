bd49aafef17ae8a2054e7d92b0f9ac9075cdf829
Zahar <zaxar.berdnikov@gmail.com>
2022-08-05 16:59:21 +0300

    dz-3

---
+++ dz 3.3.py
@@ -0,0 +1,28 @@
+"""
+Реализовать функцию get_jokes(), возвращающую n шуток, сформированных из двух случайных слов, взятых из трёх списков:
+nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
+adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
+adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
+        Например:
+get_jokes(2)
+["лес завтра зеленый", "город вчера веселый"]
+"""
+
+from random import choice
+
+nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
+adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
+adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
+
+
+def get_jokes(amount: int):
+    jokes = []
+    for i in range(amount):
+        noun = choice(nouns)
+        adverb = choice(adverbs)
+        adjective = choice(adjectives)
+        jokes.append(f'{noun} {adverb} {adjective}')
+    print(jokes)
+
+
+get_jokes(4)
